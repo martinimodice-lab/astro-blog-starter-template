1d0fa0b2a3658503850f0dfa659d50991b349ee3
Claudio <martinimodice@gmail.com>
2026-06-12 00:18:41 +0200

    Atualizar site: logo, header, menu e contato

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Claudio Martini";
+export const SITE_TITLE = "CLAUDIO MARTINI";
 export const SITE_DESCRIPTION = "Limpieza y revisión de aire acondicionado";
+export const WHATSAPP_NUMBER = "+34936120845"; // Número de WhatsApp de Claudio Martini
+export const GOOGLE_CALENDAR_URL = "https://calendar.google.com/calendar/r/eventedit?text=Agendamento+de+Servico&details=Serviço+de+limpeza+e+manutenção+de+ar+condicionado&location=&trp=false"; // Link para agendar no Google Calendar

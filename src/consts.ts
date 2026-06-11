07ab63a17b717c75782e9fc5d637dcda229c4a01
Claudio <martinimodice@gmail.com>
2026-06-12 01:53:25 +0200

    Update scheduling link to Google Calendar app URL

--- src/consts.ts
+++ src/consts.ts
@@ -4,4 +4,4 @@
 export const SITE_TITLE = "CLAUDIO MARTINI";
 export const SITE_DESCRIPTION = "Limpieza y revisión de aire acondicionado";
 export const WHATSAPP_NUMBER = "+34936120845"; // Número de WhatsApp de Claudio Martini
-export const GOOGLE_CALENDAR_URL = "https://calendar.google.com/calendar/r/eventedit?text=Agendamento+de+Servico&details=Serviço+de+limpeza+e+manutenção+de+ar+condicionado&location=&trp=false"; // Link para agendar no Google Calendar
+export const GOOGLE_CALENDAR_URL = "https://calendar.app.google/SHSrPan3wxhwiTww6"; // Link para agendar no Google Calendar
